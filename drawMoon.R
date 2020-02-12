6e768774ff11f0ee283066e99f64697c345c9947
dankelley <kelley.dan@gmail.com>
2020-02-12 15:20:56 -0400

    drawMoon() gets 'angle' argument

--- drawMoon.R
+++ drawMoon.R
@@ -1,9 +1,37 @@
+#' Rotate vectors
+#'
+#' @param p numeric data frame with `x` and `y` coordinates.
+#' @param angle numeric value giving the rotation angle in
+#' degrees, positive for clockwise rotation.
+#'
+#' @return A data frame with rotated coordinates
+#'
+#' @examples
+#' x <- 0:5
+#' y <- 0:5
+#' plot(x, y, asp=1, xlim=c(-7,7), ylim=c(-7,7))
+#' xyr <- rotateXY(x, y, 45)
+#' points(xyr$x, xyr$y, col=2, pch=20)
+#' legend("topleft", pch=c(1,20), col=c(1,2), legend=c("Unrotated", "Rotated with angle=45"))
+rotateXY <- function(p, angle=0)
+{
+    x <- p$x
+    y <- p$y
+    xy <- cbind(x, y)
+    angle <- angle * pi / 180
+    m <- cbind(c(cos(angle), sin(angle)), c(-sin(angle), cos(angle)))
+    xyr <- xy %*% m
+    data.frame(x=xyr[,1], y=xyr[,2])
+}
 #' Draw the moon showing bright and shadow regions
 #'
 #' @param phase numeric value, ranging continuously from
 #' 0 for the (dark) new moon, 1/4 for quarter moon (right side
 #' lit), 1/2 for full moon, 3/4 for three-quarter moon (left
 #' side lit, and 1 for new moon again.
+#'
+#' @param phase numeric value, giving the rotation angle, in degrees
+#' clockwise.
 #'
 #' @param lit colour used for the lit portion (defaults to a yellow).
 #'
@@ -14,7 +42,10 @@
 #' @param write logical value indicating whether to write the polygon for
 #' the lit portion to a text file with a name constructed by pasting together
 #' `phase_`, the phase to two digits, and `.dat`.
-drawMoon <- function(phase,
+#'
+#' @examples
+#' drawMoon(phase=0.15, angle=15)
+drawMoon <- function(phase, angle=0,
                      lit=rgb(200/255,150/255,10/255),
                      shadow=gray(0.5), # rgb(230/255,230/255,230/255),
                      text="",
@@ -52,7 +83,7 @@ drawMoon <- function(phase,
         xy <- XY(rep(shadowLongitude, length(lat)), lat)
         p <- data.frame(x=c(xy$x, rhs$x), y=c(xy$y, rhs$y))
     }
-    polygon(p, col=lit)
+    polygon(rotateXY(p, angle), col=lit)
     if (write)
         write.table(round(p, 5), sprintf("phase_%.4f.dat", phase), row.names=FALSE)
     if (nchar(text) > 0)
